2bbc7556c8273b13749c65b224edcfb1a6cd663f
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2017-12-10 13:50:08 +0200

    Fix page element loss
    Call selector function on hotkey press.

--- content.js
+++ content.js
@@ -1,30 +1,29 @@
 (function() {
-  function _selectPageElementByHost() {
+  function _getPageSelectorByHost() {
     const host = window.location.host
 
     switch(host) {
       case 'mangarock.com':
-        return document.getElementsByClassName('slick-slide slick-active')[0];
+        return () => document.getElementsByClassName('slick-initialized slick-slider')[0];
     }
 
     const keywords = ['manga', 'chapter', 'reader', 'anime', 'otaku', 'comics', 'cartoon'];
     if (keywords.some((keyword) => host.indexOf(keyword) !== -1)) {
-      console.log(host)
-      return document.getElementsByTagName('html')[0];
+      return () => document.getElementsByTagName('html')[0];
     }
 
     throw new RangeError(host + ' doesn\'t seem to be a reader');
   }
 
   try {
-    const page = _selectPageElementByHost();
+    const selectPage = _getPageSelectorByHost();
     var inverted = false;
 
     window.addEventListener('keydown', function(e) {
       // alt + I
       if (e.altKey && e.keyCode === 73) {
         inverted = !inverted;
-        page.style.transform = inverted
+        selectPage().style.transform = inverted
           ? 'rotate(90deg)'
           : '';
       }
