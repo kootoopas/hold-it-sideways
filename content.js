6256459a0f0821e201fd5b34a4b4508da1a67867
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2017-12-10 13:27:42 +0200

    Limit scope of hosts that script runs for

--- content.js
+++ content.js
@@ -1,24 +1,33 @@
 (function() {
-
   function _selectPageElementByHost() {
-      switch(window.location.host) {
-        case 'mangarock.com':
-          return document.getElementsByClassName('slick-slide slick-active')[0];
-        default:
-          return document.getElementsByTagName('html')[0];
-      }
+    const host = window.location.host
+
+    switch(host) {
+      case 'mangarock.com':
+        return document.getElementsByClassName('slick-slide slick-active')[0];
+    }
+
+    const keywords = ['manga', 'chapter', 'reader', 'anime', 'otaku', 'comics', 'cartoon'];
+    if (keywords.some((keyword) => host.indexOf(keyword) !== -1)) {
+      console.log(host)
+      return document.getElementsByTagName('html')[0];
+    }
+
+    throw new RangeError(host + ' doesn\'t seem to be a reader');
   }
 
-  const page = _selectPageElementByHost();
-  let inverted = false;
+  try {
+    const page = _selectPageElementByHost();
+    var inverted = false;
 
-  window.addEventListener("keydown", function(e) {
-    // alt + I
-    if (e.altKey && e.keyCode === 73) {
-      inverted = !inverted
-      page.style.transform = inverted
-        ? 'rotate(90deg)'
-        : ''
-    }
-  })
+    window.addEventListener('keydown', function(e) {
+      // alt + I
+      if (e.altKey && e.keyCode === 73) {
+        inverted = !inverted
+        page.style.transform = inverted
+          ? 'rotate(90deg)'
+          : ''
+      }
+    })
+  } catch(e) {}
 })();
