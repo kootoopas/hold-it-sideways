12df5f1a16d2dbb7e2f1183b9f54b31d4bcce8e3
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2017-12-26 16:21:32 +0200

    Switch to directional flipping w/ `,` & `.`

--- content.js
+++ content.js
@@ -16,15 +16,31 @@
     throw new RangeError(`${host} doesn't seem to be a reader`);
   }
 
+  const COMMA = 188;
+  const PERIOD = 190;
+  const flipDegrees = (degrees, keyCode) => {
+    const calc = degrees => degrees % 360;
+
+    if (keyCode === PERIOD) {
+      // flip to right
+      return calc(degrees + 90);
+    } else if (keyCode === COMMA) {
+      // flip to left
+      return calc(degrees - 90);
+    }
+
+    throw new RangeError(`keyCode is neither a comma nor a period: ${keyCode}`);
+  };
+
   try {
     const selectPage = getPageSelectorByHost();
-    let inverted = false;
 
+    let degrees = 0;
     window.addEventListener("keydown", e => {
-      // alt + I
-      if (e.altKey && e.keyCode === 73) {
-        inverted = !inverted;
-        selectPage().style.transform = inverted ? "rotate(90deg)" : "";
+      const controlPressed = e.keyCode === COMMA || e.keyCode === PERIOD;
+      if (controlPressed) {
+        degrees = flipDegrees(degrees, e.keyCode);
+        selectPage().style.transform = degrees !== 0 ? `rotate(${degrees}deg)` : "";
       }
     });
   } catch (e) {}
