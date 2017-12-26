274c8b6b56ae60f0fa23c1b465dd9e4321c1b4ba
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2017-12-26 15:56:49 +0200

    migrate to es6

--- content.js
+++ content.js
@@ -1,40 +1,31 @@
-(function() {
-  function _getPageSelectorByHost() {
-    var host = window.location.host;
+(() => {
+  function getPageSelectorByHost() {
+    const host = window.location.host;
 
-    switch(host) {
-      case 'mangarock.com':
-        return function() {
-          return document.getElementsByClassName('slick-initialized slick-slider')[0];
-        }
+    switch (host) {
+      case "mangarock.com":
+        return () => document.getElementsByClassName("slick-initialized slick-slider")[0];
     }
 
-    var keywords = ['manga', 'chapter', 'reader', 'anime', 'otaku', 'comic', 'cartoon'];
-    var hostContainsKeyword = keywords.some(function(keyword) {
-      return host.indexOf(keyword) !== -1;
-    });
-
+    const keywords = ["manga", "chapter", "reader", "anime", "otaku", "comic"];
+    const hostContainsKeyword = keywords.some(keyword => keywords.indexOf(keyword) !== -1);
     if (hostContainsKeyword) {
-      return function() {
-        return document.getElementsByTagName('html')[0];
-      }
+      return () => document.getElementsByTagName("html")[0];
     }
 
-    throw new RangeError(host + ' doesn\'t seem to be a reader');
+    throw new RangeError(`${host} doesn't seem to be a reader`);
   }
 
   try {
-    var selectPage = _getPageSelectorByHost();
-    var inverted = false;
+    const selectPage = getPageSelectorByHost();
+    let inverted = false;
 
-    window.addEventListener('keydown', function(e) {
+    window.addEventListener("keydown", e => {
       // alt + I
       if (e.altKey && e.keyCode === 73) {
         inverted = !inverted;
-        selectPage().style.transform = inverted
-          ? 'rotate(90deg)'
-          : '';
+        selectPage().style.transform = inverted ? "rotate(90deg)" : "";
       }
-    })
-  } catch(e) {}
+    });
+  } catch (e) {}
 })();
